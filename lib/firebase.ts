64435c631f9186f2a2e16554ed6ee27eba429f6f
Blythz Nexus <70764565+Blythz@users.noreply.github.com>
2025-07-26 23:56:14 +0530

    Update firebase.ts
    Firebase Config Hardcoded

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDNEmIjn_NXkOr5tRDStQBo-arwIHDYGtY",
+  authDomain: "nagaskills-8cc58.firebaseapp.com",
+  projectId: "nagaskills-8cc58",
+  storageBucket: "nagaskills-8cc58.firebasestorage.app",
+  messagingSenderId: "1049521985430",
+  appId: "1:1049521985430:web:e7f709b6257a8fa5d42fb7",
 }
 
 // Initialize Firebase
